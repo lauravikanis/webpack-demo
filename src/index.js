1c17c050d525ca735fdc87849bb4451ddaa0df79
lauravikanis <lauravikanis@yahoo.de>
2020-10-19 18:37:55 +0200

    add output management

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import _ from "lodash";
-import "./style.css";
-import Icon from "./icon.jpeg";
 
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button");
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
+  element.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
+  element.appendChild(btn);
 
   return element;
 }
